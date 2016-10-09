fb8268625b9bb198c1fd72a9b081a4a4fdb55087
Sam Thorogood <sam.thorogood@gmail.com>
2016-10-09 22:00:47 +0100

    factor out functions

--- jsbind.js
+++ jsbind.js
@@ -3,12 +3,28 @@
 (function(scope) {
 //  const nodeProto = document.registerElement('jsbind-node');
 
+  /**
+   * @param {string} value
+   * @this {Attr}
+   */
   function bindAttribute(value) {
     this.value = value;
   }
+
+  /**
+   * @param {string} value
+   * @this {Node}
+   */
   function bindTextContent(value) {
     this.textContent = value;
   }
+
+  /**
+   * Convert the passed argument to JSBind to an outer element.
+   *
+   * @param {(HTMLElement|string)} code HTMLElement, HTMLTemplateElement or string for innerHTML
+   * @return {!Node}
+   */
   function getOuter(code) {
     if (code instanceof HTMLTemplateElement) {
       return document.importNode(code.content, true);
@@ -30,48 +46,65 @@
     return fragment;
   }
 
-  window['JSBind'] = function(code, opt_data) {
+  /**
+   * @param {!Object<!Array<function(string)>>}
+   * @param {string} key
+   * @param {function(string)} helper
+   */
+  function bindingPush(binding, key, helper) {
+    let arr = binding[key];
+    if (arr === undefined) {
+      binding[key] = arr = [];
+    }
+    arr.push(helper);
+  }
+
+  const re = new RegExp(/{{(.*?)}}/g);
+
+  /**
+   * @param {string} text of HTML nodes to generate bindings for
+   * @param {!Object<!Array<function<string>>}
+   * @return {Node}
+   */
+  function convert(text, binding) {
+    const fragment = document.createDocumentFragment();
+    function appendTextNode(text) {
+      const node = document.createTextNode(text);
+      fragment.appendChild(node);
+      return node;
+    }
+
+    let atIndex = 0;
+    let match;
+    while ((match = re.exec(text)) !== null) {
+      if (match.index > atIndex) {
+        appendTextNode(text.substring(atIndex, match.index));
+        atIndex = re.lastIndex;  // step over match
+      }
+
+      const node = appendTextNode('');
+      fragment.appendChild(node);
+      bindingPush(binding, match[1], bindTextContent.bind(node));
+    }
+    re.lastIndex = NaN;  // nb. Safari doesn't like -1
+    if (!atIndex) {
+      return null;  // don't do anything, no matches
+    }
+
+    const tail = text.substring(atIndex);
+    if (tail.length) {
+      appendTextNode(tail);
+    }
+    return fragment;
+  }
+
+  /**
+   * @param {(HTMLElement|string)} code
+   * @param {*=} opt_data
+   * @return {{root: !Node, update: function(string, *)}}
+   */
+  scope['JSBind'] = function(code, opt_data) {
     const binding = {};
-    function addBinding(value, helper) {
-      let arr = binding[value];
-      if (arr === undefined) {
-        binding[value] = arr = [];
-      }
-      arr.push(helper);
-    }
-
-    const re = new RegExp(/{{(.*?)}}/g);
-    function convert(text) {
-      const fragment = document.createDocumentFragment();
-      function appendTextNode(text) {
-        const node = document.createTextNode(text);
-        fragment.appendChild(node);
-        return node;
-      }
-
-      let atIndex = 0;
-      let match;
-      while ((match = re.exec(text)) !== null) {
-        if (match.index > atIndex) {
-          appendTextNode(text.substring(atIndex, match.index));
-          atIndex = re.lastIndex;  // step over match
-        }
-
-        const node = appendTextNode('');
-        fragment.appendChild(node);
-        addBinding(match[1], bindTextContent.bind(node));
-      }
-      re.lastIndex = NaN;  // nb. Safari doesn't like -1
-      if (!atIndex) {
-        return null;  // don't do anything, no matches
-      }
-
-      const tail = text.substring(atIndex);
-      if (tail.length) {
-        appendTextNode(tail);
-      }
-      return fragment;
-    }
 
     // Traverse the entire DOM, finding insertion points.
     const outer = getOuter(code);
@@ -82,7 +115,7 @@
 
       if (n instanceof Text) {
         // TODO: matches e.g. any HTMLElement, although doesn't do anything
-        const out = convert(n.wholeText);
+        const out = convert(n.wholeText, binding);
         if (out) {
           n.parentNode.replaceChild(out, n);
         }
@@ -100,7 +133,7 @@
         for (const attr in found) {
           n.removeAttribute(attr + '$');
           n.setAttribute(attr, '');
-          addBinding(found[attr], bindAttribute.bind(n.attributes[attr]));
+          bindingPush(binding, found[attr], bindAttribute.bind(n.attributes[attr]));
         }
       }
     }
